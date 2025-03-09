7185a3be800bce1064e6fb96bd23cb3a3c3df8ed
Abdulkabir fagbohun <122563248+abdulkabir18@users.noreply.github.com>
2025-03-09 22:28:29 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int[] userArray = { 1, 2, 3, 4, 5 };
+int find = 4;
+
+int result = Recursion_BinarySearch(userArray, find, 0, userArray.Length - 1);
+
+Console.WriteLine(result == -1 ? $"Target: {find} is not found in the collection" : $"Target: {find} is found at index of {result}");
+
+int Recursion_BinarySearch(int[] myArray, int target, int firstIndex, int lastIndex)
+{
+    int middleIndex;
+
+    if (firstIndex > lastIndex)
+    {
+        return -1;
+    }
+
+    middleIndex = (firstIndex + lastIndex) / 2;
+
+    if (myArray[middleIndex] == target)
+    {
+        return middleIndex;
+    }
+    else if (target > myArray[middleIndex])
+    {
+        return Recursion_BinarySearch(myArray, target, middleIndex + 1, lastIndex);
+    }
+    else
+        return Recursion_BinarySearch(myArray, target, firstIndex, middleIndex - 1);
+
+}

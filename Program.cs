a4170880f861d9e25fde8508d2ea4540e633ee1b
Abdulkabir fagbohun <122563248+abdulkabir18@users.noreply.github.com>
2025-03-09 22:41:03 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
-﻿int[] userArray = { 1, 2, 3, 4, 5 };
+// Texting Values
+int[] userArray = { 1, 2, 3, 4, 5 };
 int find = 4;
 
+// Invoking or Calling of the method
 int result = Recursion_BinarySearch(userArray, find, 0, userArray.Length - 1);
 
+// Printing the result to the console
 Console.WriteLine(result == -1 ? $"Target: {find} is not found in the collection" : $"Target: {find} is found at index of {result}");
 
+// Declearation of method
 int Recursion_BinarySearch(int[] myArray, int target, int firstIndex, int lastIndex)
 {
     int middleIndex;
 
+    // Base case
     if (firstIndex > lastIndex)
     {
         return -1;
     }
-
+    
     middleIndex = (firstIndex + lastIndex) / 2;
 
     if (myArray[middleIndex] == target)
@@ -22,9 +27,13 @@ int Recursion_BinarySearch(int[] myArray, int target, int firstIndex, int lastIn
     }
     else if (target > myArray[middleIndex])
     {
+        // Recursive Call
         return Recursion_BinarySearch(myArray, target, middleIndex + 1, lastIndex);
     }
     else
+        // Recursive Call
         return Recursion_BinarySearch(myArray, target, firstIndex, middleIndex - 1);
 
 }
+
+// This is how recursion works in the context of binary search, breaking the problem into smaller subproblems each time.
